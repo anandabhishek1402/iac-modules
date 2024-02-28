7a4de3604063423126a73796022e93fcd90eaabb
anandabhishek1402 <154116864+anandabhishek1402@users.noreply.github.com>
2024-02-28 15:50:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,21 +4,21 @@ variable "pipeline_name"{
 variable "team_tag"{
     type = string
 }
-variable "stages"{
-    type = list(object({
+# variable "stages"{
+  #   type = list(object({
 
-        skaffold_profile_name = string
-        target = object({
-            type = string
-            name = string
-            project = string
-            deploy_service_account_email = string
-            artifact_storage = string
-            membership = string
-            location = string
-        })
-        require_approval = bool
+#         skaffold_profile_name = string
+  #       target = object({
+    #         type = string
+      #       name = string
+        #     project = string
+#             deploy_service_account_email = string
+  #           artifact_storage = string
+    #         membership = string
+      #       location = string
+        # })
+        # require_approval = bool
 
-    }))
-    default = []
-}
+#     }))
+  #   default = []
+# }
